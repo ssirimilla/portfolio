c904c2fb4289de8830f329c2b46111a5877d5938
Sujal Sirimilla <ssirimilla@ucsd.edu>
2026-04-17 23:31:16 -0700

    pls2

--- global.js
+++ global.js
@@ -6,9 +6,10 @@ function $$(selector, context = document) {
 
 // ---------------- NAV ----------------
 let pages = [
-  { url: "", title: "Home" },
-  { url: "projects/", title: "Projects" },
-  { url: "contact/", title: "Contact" },
+  { url: "/", title: "Home" },
+  { url: "/projects/", title: "Projects" },
+  { url: "/contact/", title: "Contact" },
+  { url: "/resume/", title: "Resume" },
   { url: "https://github.com/ssirimilla", title: "GitHub" },
 ];
 
@@ -21,7 +22,7 @@ let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
+  let url = p.url;
 
   let a = document.createElement("a");
   a.href = url;
@@ -37,3 +38,23 @@ for (let p of pages) {
   nav.append(a);
 }
 
+let select = document.createElement("select");
+select.className = "color-scheme";
+
+select.innerHTML = `
+  <option value="light dark">Auto</option>
+  <option value="light">Light</option>
+  <option value="dark">Dark</option>
+`;
+
+select.value = localStorage.colorScheme || "light dark";
+
+select.addEventListener("change", () => {
+  document.documentElement.style.colorScheme = select.value;
+  localStorage.colorScheme = select.value;
+});
+
+// apply saved preference on load
+document.documentElement.style.colorScheme = select.value;
+
+document.body.append(select);
