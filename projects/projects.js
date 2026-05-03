60f601b03e72e612e35035899976c9f154a3e5f4
Sujal Sirimilla <ssirimilla@ucsd.edu>
2026-05-02 21:49:57 -0700

    lab5

--- projects/projects.js
+++ projects/projects.js
@@ -10,3 +10,25 @@ const title = document.querySelector(".projects-title");
 
 title.textContent = `${projects.length} Projects`;
 
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let sliceGenerator = d3.pie();
+
+let arcData = sliceGenerator(data);
+
+let arcs = arcData.map(d => arcGenerator(d));
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
